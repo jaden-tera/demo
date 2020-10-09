aae82b140b87fba847f647c52608eee20b510ce7
joo-pe <joohyung1002@naver.com>
2020-10-10 08:00:35 +0900

    RepositoryTest add

---
+++ src/test/java/com/example/demo/repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
